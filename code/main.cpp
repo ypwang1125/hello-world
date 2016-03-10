c3a77485db22faae1109005e01b9cc0cccf1edef
= <ypwang.tamu@gmail.com>
2016-03-10 16:12:53 -0600

    anotherFileInNewFolder

---
+++ code/main.cpp
@@ -0,0 +1,16 @@
+#include<ilocplex/ilocplex.h>
+#include<iostream>
+#include<vector>
+
+ILOSTLBEGIN
+using namespace std;
+using namespace arma; 
+
+int main(int argc, char **argv)
+{
+   cout << "Hello World!" << endl; 
+   
+   return 0; 
+
+}
+
